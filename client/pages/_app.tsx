e7ee7fe430a13cf86779ae6f1787016ff35a1696
Rodrigo <rvena@freecast.com>
2023-11-13 12:44:43 -0500

    cleaned default assets, updated favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from "../components/Navbar";
@@ -5,6 +6,9 @@ import Navbar from "../components/Navbar";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-40% from-gray-900 to-pink-900">
         <Navbar/>
         <Component {...pageProps} />
